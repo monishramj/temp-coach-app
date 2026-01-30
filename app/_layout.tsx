f453fb36188ec363730935aabfcf9f6376c98c94
monishramj <monishramj621@gmail.com>
2026-01-30 02:59:28 -0500

    added NativeWind for styling + update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+import "../global.css";
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
 
 export const unstable_settings = {
   anchor: '(tabs)',
